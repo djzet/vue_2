4ed57839bb1bc9ca7d2a229e93730c97d4cb7247
Djzet <silaevegor2003@mail.ru>
2023-03-09 14:50:21 +0700

    component notes and mounted

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+let eventBus = new Vue();
 
+Vue.component('notes',{
+    template:`
+    `,
+    data(){
+        return{
+            colum_1: [],
+            colum_2: [],
+            colum_3: [],
+            errors: []
+        }
+    },
+    mounted(){
+        eventBus.$on('notes-submitted', notes =>{
+            this.errors = []
+            if (this.colum_1.length < 3){
+                this.colum_1.push(notes);
+            }else {
+                this.errors.push('Unable to create new entry');
+            }
+        })
+    },
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+       name: 'Notes'
+    }
+})
